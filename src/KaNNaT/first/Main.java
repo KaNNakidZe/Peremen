97335dfb6d988626869d5932768b30b6cbffee30
KaNNaT <KaNNaT13@yandex.ru>
2022-03-28 21:46:58 +0300

    Initial commit

---
+++ src/KaNNaT/first/Main.java
@@ -0,0 +1,20 @@
+package KaNNaT.first;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    var box = 5;
+    System.out.println(box);
+    box = box + 2;
+    System.out.println(box);
+    box = box - 3;
+    System.out.println(box);
+    box = box * 3;
+    System.out.println(box);
+    box = box / 4;
+    System.out.println(box);
+
+    
+    }
+}
